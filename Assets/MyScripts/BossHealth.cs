24998cf8be9a30b1de0784e6c7bff39f30a79818
benxta4424 <podean.beni@gmail.com>
2026-01-15 21:52:18 +0200

    adding the second boss and its attack source (minion spawning)

--- Assets/MyScripts/BossHealth.cs
+++ Assets/MyScripts/BossHealth.cs
@@ -3,12 +3,12 @@ using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
-    // Mărim viața la 5000 pentru ca 1 damage să fie insignifiant (0.02%)
     public int maxHealth = 5000; 
-    private int currentHealth;
+    protected int currentHealth; // protected permite accesul scripturilor care moștenesc
     public Slider hpSlider;
 
-    void Start()
+    // Start trebuie să fie virtual pentru a fi extins
+    protected virtual void Start()
     {
         currentHealth = maxHealth;
         if (hpSlider != null)
@@ -18,17 +18,17 @@ public class BossHealth : MonoBehaviour
         }
     }
 
-    public void TakeDamage(int damage)
+    // virtual permite noului boss să își scrie propria logică de damage
+    public virtual void TakeDamage(int damage)
     {
         currentHealth -= damage;
         if (hpSlider != null) hpSlider.value = currentHealth;
-
         if (currentHealth <= 0) Die();
     }
 
-    void Die()
+    protected void Die()
     {
-        Debug.Log("Boss-ul a fost învins!");
+        Debug.Log(gameObject.name + " a fost învins!");
         Destroy(gameObject);
     }
 }
